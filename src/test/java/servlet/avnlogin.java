5949b0f4dd08035fe2f883f8fcd0578c7efe4669
deeptigupta0013 <43944772+deeptigupta0013@users.noreply.github.com>
2018-10-08 17:14:09 +0530

    Update avnlogin.java

--- src/test/java/servlet/avnlogin.java
+++ src/test/java/servlet/avnlogin.java
@@ -15,6 +15,7 @@ public class avnlogin extends HttpServlet {
 	            throws ServletException, IOException {
 	        response.setContentType("text/html;charset=UTF-8");
 	        	        
+		
 	        String username = request.getParameter("username");
 	        String userpassword = request.getParameter("userpassword");
 	       	        
